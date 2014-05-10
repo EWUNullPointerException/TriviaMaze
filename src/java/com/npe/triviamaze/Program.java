f71dbc3876f80094ca3af7eb56cdc06c614b26bd
Stefan Bostain <zilulil@gmail.com>
2014-05-10 13:23:22 -0700

    Fixed position of ExitButton

--- src/java/com/npe/triviamaze/Program.java
+++ src/java/com/npe/triviamaze/Program.java
@@ -7,19 +7,20 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
 
 public class Program
 {
     private static final int WIDTH = 361;
     private static final int HEIGHT = 348;
     private static Shell shell;
-    
 
     public static void main(String[] args)
     {
         Display display = new Display();
         shell = new Shell(display);
-        
+
         // Dan
         // must be called before setSize is called to make sure
         // the components are placed correctly for some odd reason.
@@ -50,9 +51,13 @@ public class Program
     private static void init()
     {
         // Dan
-        int cWidth, cHeight;
-        
+
         Button exitBtn = new Button(shell, SWT.PUSH);
+        FormData fd_exitBtn = new FormData();
+        fd_exitBtn.top = new FormAttachment(0);
+        fd_exitBtn.left = new FormAttachment(0);
+        exitBtn.setLayoutData(fd_exitBtn);
+
         // Adds functionality to the button by adding a listener for
         // being clicked, or as SWT has defined it, for "selection"
         exitBtn.addListener(SWT.Selection, new Listener()
@@ -61,19 +66,13 @@ public class Program
             {
                 // Not sure if this conditional is needed as it seems
                 // to work as expected without it.
-                //if(event.type == SWT.Selection)
-                    shell.dispose();
+                // if(event.type == SWT.Selection)
+                shell.dispose();
             }
         });
         exitBtn.setText("Exit");
         // Resize the control before moving it.
         exitBtn.pack();
-        // I don't think getSize() works quite how it intuitively
-        // sounds given subtracting the width and height (without the -50)
-        // should put it tucked into the bottom right corner... but it doesn't.
-        cWidth = WIDTH - exitBtn.getSize().x - 50;
-        cHeight = HEIGHT - exitBtn.getSize().y - 50;
-        exitBtn.setLocation(cWidth, cHeight);
-        // EndDan
+
     }
 }
