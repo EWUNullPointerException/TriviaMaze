ae765f6771c32b7baca85c865e086422265d1e95
Stefan Bostain <zilulil@gmail.com>
2014-06-05 18:48:50 -0700

    Fixes to export properly

--- src/test/java/GameTests.java
+++ src/test/java/GameTests.java
@@ -6,9 +6,6 @@ import com.npe.triviamaze.game.Direction;
 import com.npe.triviamaze.game.Game;
 import com.npe.triviamaze.game.Location;
 
-
-
-
 public class GameTests
 {
 
@@ -18,14 +15,14 @@ public class GameTests
         Game game = new Game(1, 1);
         assertTrue("Game of 1 room is instantly won", game.isGameWon());
     }
-    
+
     @Test
     public void testCreateMazes()
     {
         Game game = new Game(1, 2);
         assertFalse("Game of size 1x2 is not instantly won", game.isGameWon());
     }
-    
+
     @Test
     public void testCanMove()
     {
@@ -34,25 +31,26 @@ public class GameTests
         assertFalse("Cannot move in game of size 1", game.canMove(Direction.Right));
         assertFalse("Cannot move in game of size 1", game.canMove(Direction.Up));
         assertFalse("Cannot move in game of size 1", game.canMove(Direction.Down));
-        
+
         game = new Game(2, 2);
         assertFalse("Cannot move left from start", game.canMove(Direction.Left));
         assertFalse("Cannot move up from start", game.canMove(Direction.Up));
         assertTrue("Can move right", game.canMove(Direction.Right));
         assertTrue("Can move down", game.canMove(Direction.Down));
     }
-    
+
     @Test
     public void testMove()
     {
         Game game = new Game(2, 2);
         assertTrue("Can move right from start", game.move(Direction.Right));
-        assertTrue("Actually moved right", game.getPlayer().getLocation().equals(new Location(1, 2)));
+        assertTrue("Actually moved right", game.getPlayer().getLocation()
+                .equals(new Location(1, 2)));
         assertFalse("Cannot move right again", game.canMove(Direction.Right));
         assertTrue("Can move down", game.move(Direction.Down));
         assertTrue("Have won game", game.isGameWon());
     }
-    
+
     @Test
     public void testisGameOver()
     {
